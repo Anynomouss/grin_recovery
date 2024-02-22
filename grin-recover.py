86a517ea152fa8f40b7977860491a55e36cadd17
Anynomouss <85170856+Anynomouss@users.noreply.github.com>
2024-02-22 16:35:50 +0100

    Update grin-recover.py
    Made python paths defaault

--- grin-recover.py
+++ grin-recover.py
@@ -13,18 +13,18 @@ if __name__ == '__main__':
     1) Copy the wallet you want to recover the password for into the same directory
     as the script. It is assumed your wallet has the name "wallet.seed"
     2) Run the script with your passwords list as input, see examples below:
-     - time cat password_list.txt ../../../Python/Python311/python.exe grin-recover.py
+     - time cat password_list.txt python.exe grin-recover.py
     3) A - Benchmark the speed, measure time it takes to test 10000 passwords single core
-     - time printf 'HelloWorld\n%.0s' {1..10000} | ../../Python/Python311/python.exe grin-recover_0.1.py  
+     - time printf 'HelloWorld\n%.0s' {1..10000} | python grin-recover_0.1.py  
     3) B - Benchmark in parallel, settings are chose for 16 thread Ryzen 7
-     - time printf 'HelloWorld\n%.0s' {1..100000} |  parallel --pipe -j 16 --blocksize 100000 --spreadstdin ../../Python/Python311/python.exe grin-recover.py
+     - time printf 'HelloWorld\n%.0s' {1..100000} |  parallel --pipe -j 16 --blocksize 100000 --spreadstdin python grin-recover.py
      
      BENCHMARK RESULTS (Ryzen 7):
      - single core 6.693 pwds/second
      - 16 thread 40.0000 pwds/second
      
     3) C - Example using hashcat output with multithreading
-    ./hashcat.exe -a1 words.txt endings.txt --stdout |  parallel --pipe -j 16 --blocksize 100000 --spreadstdin ../../Python/Python311/python.exe grin-recover.py
+    ./hashcat.exe -a1 words.txt endings.txt --stdout |  parallel --pipe -j 16 --blocksize 100000 --spreadstdin python grin-recover.py
     '''
     target_keystore_file = "wallet.seed"
 
